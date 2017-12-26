2e1a3c99c75a0ba5fc99dff1f7df08891ddf39d6
Chris Isherwood <cisherwood321@gmail.com>
2017-12-26 18:00:10 -0500

    Initial migrations Connection string Azure DB Credentials

--- investrs/Data/Migrations/20171226225632_initial.Designer.cs
+++ investrs/Data/Migrations/20171226225632_initial.Designer.cs
@@ -8,9 +8,10 @@ using investrs.Data;
 namespace investrs.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171226225632_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
